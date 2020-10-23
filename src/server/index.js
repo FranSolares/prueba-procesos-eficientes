df27f0acda77b118d2b9bfe1c73c0dea21336e8d
FranSolares <fsolaresdev@gmail.com>
2020-10-22 18:54:08 -0600

    Added routing for vehicles

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const vehicleRoutes = require('./routes/vehicles.routes')
 const app = express()
+
+app.use('/vehicles', vehicleRoutes)
 
 const PORT = process.env.SERVER_PORT || 3500
 
--- src/server/routes/vehicles.routes.js
+++ src/server/routes/vehicles.routes.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const {insertVehicle, updateVehicle, deleteVehicle, selectVehicles} = require('../controllers/vehicles.controller')
 
 const Router = express.Router()
 
-Router.post()
+Router.post('/insert', insertVehicle)
+Router.put('/update', updateVehicle)
+Router.delete('/delete', deleteVehicle)
+Router.get('/select', selectVehicles)
+
+module.exports = Router
